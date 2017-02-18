69dbbfa50ba10466386edf3bb590cd1586bdc2a8
azylbertal <asaph.zylbertal@mail.huji.ac.il>
2017-02-18 15:18:06 +0200

    fixed

--- sensors_stream.py
+++ sensors_stream.py
@@ -7,18 +7,62 @@ import multiprocessing
 import pyaudio
 import struct
 import RPi.GPIO as io
+import spidev
+import Adafruit_LSM303
 from time import sleep
+from smbus import SMBus
 
-FORWARD_LEFT = 17
-BACKWARD_LEFT = 18
-FORWARD_RIGHT = 23
-BACKWARD_RIGHT = 22
+busNum = 1
+b = SMBus(busNum)
+LGD = 0x6b #Device I2C slave address
+LGD_WHOAMI_ADDRESS = 0x0F
+LGD_WHOAMI_CONTENTS = 0b11010111 #Device self-id
+
+LGD_CTRL_1 = 0x20 #turns on gyro
+LGD_CTRL_2 = 0x21 #can set a high-pass filter for gyro
+LGD_CTRL_3 = 0x22
+LGD_CTRL_4 = 0x23
+LGD_CTRL_5 = 0x24
+LGD_CTRL_6 = 0x25
+
+LGD_TEMP = 0x26
+
+#Registers holding gyroscope readings
+LGD_GYRO_X_LSB = 0x28
+LGD_GYRO_X_MSB = 0x29
+LGD_GYRO_Y_LSB = 0x2A
+LGD_GYRO_Y_MSB = 0x2B
+LGD_GYRO_Z_LSB = 0x2C
+LGD_GYRO_Z_MSB = 0x2D
+
+FORWARD_LEFT = 18
+BACKWARD_LEFT = 17
+FORWARD_RIGHT = 22
+BACKWARD_RIGHT = 23
 
 CAMERA_TCP_PORT = 5005
 MIC_TCP_PORT = 5006
 LEFT_MOTOR_PORT = 5007
 RIGHT_MOTOR_PORT = 5008
+SENS_TCP_PORT = 5009
+
 BUFFER_SIZE = 1024
+
+SETTINGS_FILE = "RTIMULib"
+
+def twos_comp_combine(msb, lsb):
+    twos_comp = 256*msb + lsb
+    if twos_comp >= 32768:
+        return twos_comp - 65536
+    else:
+        return twos_comp
+
+def getGyro():
+    gyrox = twos_comp_combine(b.read_byte_data(LGD, LGD_GYRO_X_MSB), b.read_byte_data(LGD, LGD_GYRO_X_LSB))
+    gyroy = twos_comp_combine(b.read_byte_data(LGD, LGD_GYRO_Y_MSB), b.read_byte_data(LGD, LGD_GYRO_Y_LSB))
+    gyroz = twos_comp_combine(b.read_byte_data(LGD, LGD_GYRO_Z_MSB), b.read_byte_data(LGD, LGD_GYRO_Z_LSB))
+
+    return (gyrox, gyroy, gyroz)
 
 def camera_stream():
 
@@ -65,6 +109,7 @@ def camera_stream():
         pixArray[:, :, 1] = pixArray[:, :, 2]
         np.copyto(img_buffer, pixArray[:, :, 2])
         dat = img_buffer.tostring()
+
         s.send(dat)
     s.close()
 
@@ -87,13 +132,13 @@ def mic_stream():
     
     
     while True:
-        s.recv(1)
+        if s.recv(1)=='c': break
         a = stream.read(audio_bin, exception_on_overflow = False)
         b = np.array(struct.unpack('<' + str(audio_bin) + 'h', a), dtype='int16')
         dat = b.tostring()
         
         s.send(dat)
-
+    s.close()
 
 def motor_control():
 
@@ -103,74 +148,149 @@ def motor_control():
     io.setup(FORWARD_RIGHT, io.OUT)
     io.setup(BACKWARD_RIGHT, io.OUT)
 
-    forward_left_pwm = io.PWM(FORWARD_LEFT, 500)
-    backward_left_pwm = io.PWM(BACKWARD_LEFT, 500)
-    forward_right_pwm = io.PWM(FORWARD_RIGHT, 500)
-    backward_right_pwm = io.PWM(BACKWARD_RIGHT, 500)
+#    forward_left_pwm = io.PWM(FORWARD_LEFT, 500)
+#    backward_left_pwm = io.PWM(BACKWARD_LEFT, 500)
+#    forward_right_pwm = io.PWM(FORWARD_RIGHT, 500)
+#    backward_right_pwm = io.PWM(BACKWARD_RIGHT, 500)
 
-    forward_left_pwm.start(0)
-    backward_left_pwm.start(0)
-    forward_right_pwm.start(0)
-    backward_right_pwm.start(0)
-
+#    forward_left_pwm.start(0)
+#    backward_left_pwm.start(0)
+#    forward_right_pwm.start(0)
+#    backward_right_pwm.start(0)
     s_left = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s_left.connect((TCP_IP, LEFT_MOTOR_PORT))
+    sleep(1)
     s_right = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s_right.connect((TCP_IP, RIGHT_MOTOR_PORT))
-
+    print 'connected to motor server'
     while True:
-        left_dat=s_left.recv(8)
-        right_dat=s_right.recv(8)
-	if left_dat == None or right_dat == None:
+        left_dat=s_left.recv(2)
+        right_dat=s_right.recv(2)
+	
+	if left_dat == None or right_dat == None or len(left_dat)==0 or len(right_dat)==0:
 	    break
-        left_power=struct.unpack("d", left_dat)[0]
-        right_power=struct.unpack("d", right_dat)[0]
-
-        if abs(left_power) > 0.00001:
+        left_power=struct.unpack("H", left_dat)[0]
+        right_power=struct.unpack("H", right_dat)[0]
+        if abs(left_power) > 0:
             if left_power > 0:
-                if left_power > 100:
-                    left_power = 100
-                backward_left_pwm.ChangeDutyCycle(0)
-                forward_left_pwm.ChangeDutyCycle(int(left_power))
+#                if left_power > 100:
+#                    left_power = 100
+#                backward_left_pwm.ChangeDutyCycle(0)
+#                forward_left_pwm.ChangeDutyCycle(int(left_power))
+		io.output(BACKWARD_LEFT, 0)
+		io.output(FORWARD_LEFT, 1)
             else:
-                if left_power < -100:
-                    left_power = -100
-                forward_left_pwm.ChangeDutyCycle(0)
-                backward_left_pwm.ChangeDutyCycle(-int(left_power))
-        else:
-            forward_left_pwm.ChangeDutyCycle(0)
-            backward_right_pwm.ChangeDutyCycle(0)
+#                if left_power < -100:
+#                    left_power = -100
+#                forward_left_pwm.ChangeDutyCycle(0)
+#                backward_left_pwm.ChangeDutyCycle(-int(left_power))
+		io.output(FORWARD_LEFT, 0)
+		io.output(BACKWARD_LEFT, 1)
 
-        if abs(right_power) > 0.00001:
-            if right_power > 0:
-                if right_power > 100:
-                    right_power = 100
-                backward_right_pwm.ChangeDutyCycle(0)
-                forward_right_pwm.ChangeDutyCycle(int(right_power))
-            else:
-                if right_power < -100:
-                    right_power = -100
-                forward_right_pwm.ChangeDutyCycle(0)
-                backward_right_pwm.ChangeDutyCycle(-int(right_power))
         else:
-            forward_right_pwm.ChangeDutyCycle(0)
-            backward_right_pwm.ChangeDutyCycle(0)
+#            forward_left_pwm.ChangeDutyCycle(0)
+#            backward_right_pwm.ChangeDutyCycle(0)
+	    io.output(FORWARD_LEFT, 0)
+	    io.output(BACKWARD_LEFT, 0)
+
+        if abs(right_power) > 0:
+            if right_power > 0:
+#                if right_power > 100:
+#                    right_power = 100
+#                backward_right_pwm.ChangeDutyCycle(0)
+#                forward_right_pwm.ChangeDutyCycle(int(right_power))
+		io.output(BACKWARD_RIGHT, 0)
+		io.output(FORWARD_RIGHT, 1)
+            else:
+#                if right_power < -100:
+#                    right_power = -100
+#                forward_right_pwm.ChangeDutyCycle(0)
+#                backward_right_pwm.ChangeDutyCycle(-int(right_power))
+		io.output(FORWARD_RIGHT, 0)
+		io.output(BACKWARD_RIGHT, 1)
+        else:
+	    io.output(FORWARD_RIGHT, 0)
+	    io.output(BACKWARD_RIGHT, 0)
+
+#            forward_right_pwm.ChangeDutyCycle(0)
+#            backward_right_pwm.ChangeDutyCycle(0)
+
+    io.output(FORWARD_LEFT, 0)
+    io.output(BACKWARD_LEFT, 0)
+    io.output(FORWARD_RIGHT, 0)
+    io.output(BACKWARD_RIGHT, 0)
+#    forward_right_pwm.stop()
+#    forward_left_pwm.stop()
+#    backward_right_pwm.stop()
+#    backward_left_pwm.stop()
+
     s_left.close()
     s_right.close()
     io.cleanup()
             
-s_broad = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s_broad.bind(('', 50000))
+
+def sensors_str():
+
+    if b.read_byte_data(LGD, LGD_WHOAMI_ADDRESS) == LGD_WHOAMI_CONTENTS:
+        print 'L3GD20H detected successfully on I2C bus '+str(busNum)+'.'
+    else:
+        print 'No L3GD20H detected on bus on I2C bus '+str(busNum)+'.'
+    b.write_byte_data(LGD, LGD_CTRL_1, 0x0F)
+
+    lsm303 = Adafruit_LSM303.LSM303()
+
+    spi = spidev.SpiDev()
+    spi.open(0, 0)
+    print 'connecting to sensor server'
+    s_sens = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_sens.connect((TCP_IP, SENS_TCP_PORT))
+    print 'connected to sensor server'
+    while True:
+
+	req=s_sens.recv(1)
+        if req=='c':
+	    break
+        adc = spi.xfer2([1, (8 + 0) << 4, 0])
+        prox_data = ((adc[1] & 3) << 8) + adc[2]
+        prox_str = struct.pack("H", prox_data)
+
+	(accel, mag) = lsm303.read()
+	gyro = getGyro()
+	gyrox_str = struct.pack("f", gyro[0])        
+	gyroy_str = struct.pack("f", gyro[1])        
+	gyroz_str = struct.pack("f", gyro[2])        
+
+	accelx_str = struct.pack("f", accel[0])        
+	accely_str = struct.pack("f", accel[1])        
+	accelz_str = struct.pack("f", accel[2])        
+
+	magnetx_str = struct.pack("f", mag[0])        
+	magnety_str = struct.pack("f", mag[1])        
+	magnetz_str = struct.pack("f", mag[2])        
+
+	s_sens.send(prox_str + gyrox_str + gyroy_str + gyroz_str + accelx_str + accely_str + accelz_str + magnetx_str + magnety_str + magnetz_str)
+        sleep(0.01)
+    s_sens.close()
+
+
+#s_broad = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#s_broad.bind(('', 50000))
 
 print 'waiting for server'
-TCP_IP, wherefrom = s_broad.recvfrom(1500, 0)
+#TCP_IP, wherefrom = s_broad.recvfrom(1500, 0)
+TCP_IP = '10.42.0.1'
 print 'server ip:'+TCP_IP
-sleep(2)
 
 cam_proc = multiprocessing.Process(target = camera_stream, args = ())
-#mic_proc = multiprocessing.Process(target = mic_stream, args = ())
-#motor_proc = multiprocessing.Process(target = motor_control, args = ())
+mic_proc = multiprocessing.Process(target = mic_stream, args = ())
+motor_proc = multiprocessing.Process(target = motor_control, args = ())
+sensors_proc = multiprocessing.Process(target = sensors_str, args = ())
 
+sleep(1)
 cam_proc.start()
-#mic_proc.start()
-#motor_proc.start()
+sleep(1)
+mic_proc.start()
+sleep(1)
+motor_proc.start()
+sleep(2.5)
+sensors_proc.start()
